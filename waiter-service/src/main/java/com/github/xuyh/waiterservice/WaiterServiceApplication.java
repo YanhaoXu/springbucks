212dc9df6ac999d799486c117a5e736971be94c5
yanhao.xu <yanhao.xu@outlook.com>
2022-02-21 21:37:27 +0800

    waiter-service init

---
+++ waiter-service/src/main/java/com/github/xuyh/waiterservice/WaiterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.xuyh.waiterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiterServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WaiterServiceApplication.class, args);
+  }
+
+}
---
+++ waiter-service/src/test/java/com/github/xuyh/waiterservice/WaiterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xuyh.waiterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiterServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
