9afa96380854f15b1de05c833398c6bd0b5cfaf1
yanhao.xu <yanhao.xu@outlook.com>
2022-03-21 23:52:30 +0800

    feature(waiter-service): update

--- waiter-service/src/main/java/com/github/xuyh/waiterservice/WaiterServiceApplication.java
+++ waiter-service/src/main/java/com/github/xuyh/waiterservice/WaiterServiceApplication.java
@@ -1,13 +1,33 @@
 package com.github.xuyh.waiterservice;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
-public class WaiterServiceApplication {
+public class WaiterServiceApplication implements WebMvcConfigurer {
 
   public static void main(String[] args) {
     SpringApplication.run(WaiterServiceApplication.class, args);
   }
 
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    //    registry.addInterceptor(new PerformanceInteceptor)
+  }
+
+  @Bean
+  public Hibernate5Module hibernate5Module() {
+    return new Hibernate5Module();
+  }
+
+  public Jackson2ObjectMapperBuilderCustomizer jacksonBuilderCustomizer() {
+    return builder -> builder.indentOutput(true).timeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+  }
 }
